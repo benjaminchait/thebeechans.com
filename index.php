c731be8303d05ff918bc1480beb2f3969aca054b
Benjamin Chait <hello@benjaminchait.net>
2018-05-29 20:27:00 -0700

    Initial commit from Samantha Felge

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link type="text/css" href="/css/reset.css" rel="stylesheet" />
+	<link type="text/css" href="/css/style.css" rel="stylesheet" />
+
+	<title>staging.place</title>
+
+</head>
+
+<body>
+<a href="http://brentspore.com"><img src="mightyarmy_banner.svg" class="ma_banner"/></a>
+</body>
+</html>
